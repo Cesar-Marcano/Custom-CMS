e99746784398dafb95bbc9201fef6517b4ba2561
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-03 17:56:50 -0400

    created main user service functions

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
   providers: [UserService],
 })
