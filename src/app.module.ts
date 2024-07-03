289e530e06d1010d8760b71e3377dc1b1bfec6de
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-03 17:46:01 -0400

    created necessary app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PostModule, CommentModule, AuthModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
