01465c646a687da0f119e88cd37b2ee3f62bd10b
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-04 14:01:55 -0400

    created register controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { AuthService } from './auth.service';
+import { User } from '@prisma/client';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('register')
+  public async register(@Body() data: CreateUserDto): Promise<User> {
+    return await this.authService.register(data);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { RoleService } from 'src/user/role.service';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/logInUser.dto';
 
 @Injectable()
 export class AuthService {
@@ -18,5 +19,18 @@ export class AuthService {
     data.password = hash;
 
     return await this.user.createUser(data);
+  }
+
+  public async login({ email, password }: LoginDto) {
+    const user = await this.user.user({
+      email,
+    });
+
+    const arePasswordsEqual = await bcrypt.compare(password, user.password);
+
+    if (!arePasswordsEqual)
+      throw new UnauthorizedException('The password provided was incorrect.');
+
+    return user;
   }
 }
