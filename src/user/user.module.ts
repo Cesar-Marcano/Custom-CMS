964a1a9e3de0152d6887bb4c68f5dd18558989e2
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-04 14:45:01 -0400

    removed useless UserController in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { RoleService } from './role.service';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [UserController],
   providers: [UserService, RoleService],
   exports: [UserService, RoleService],
 })
