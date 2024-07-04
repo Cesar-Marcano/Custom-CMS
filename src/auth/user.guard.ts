6ac5e679ad3a3dae32cd31cae97adce24d37b3d0
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-04 18:53:16 -0400

    created user guard

---
+++ src/auth/user.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
+import { jwtPayload } from 'src/auth/auth.service';
+import { Role } from '@prisma/client';
+
+@Injectable()
+export class UserGuard extends AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isAuth = await super.canActivate(context);
+    if (!isAuth) {
+      return false;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user: jwtPayload = request.user;
+
+    if (user.role === Role.ADMIN || user.role === Role.USER) {
+      return true;
+    } else {
+      throw new UnauthorizedException(
+        'You are not authorized to access this resource.',
+      );
+    }
+  }
+}
