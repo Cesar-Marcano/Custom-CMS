ed476769fbf3f7b367f48905510a99e2c55e4988
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-05 18:18:57 -0400

    created fetch comments routes

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { Comment } from '@prisma/client';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+  constructor(private readonly comment: CommentService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  public async getComment(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Comment> {
+    return await this.comment.comment({ id });
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('post/:slug')
+  public async getPostComment(@Param('slug') slug: string): Promise<Comment[]> {
+    return await this.comment.comments({ post: { slug } });
+  }
+}
