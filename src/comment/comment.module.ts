a4597b91fd87972d321a3552d788c987d92f8bfd
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-05 17:35:25 -0400

    created fetch comments functions

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [CommentService],
   controllers: [CommentController],
 })
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Comment, Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(private prisma: DatabaseService) {}
+
+  public async comment(
+    where: Prisma.CommentWhereUniqueInput,
+  ): Promise<Comment> {
+    const comment = await this.prisma.comment.findUnique({ where });
+
+    if (!comment) throw new NotFoundException('Comment not found.');
+
+    return comment;
+  }
+
+  public async comments(params: {
+    post: Prisma.PostWhereUniqueInput;
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.CommentWhereUniqueInput;
+    where?: Prisma.CommentWhereInput;
+    orderBy?: Prisma.CommentOrderByWithRelationInput;
+  }): Promise<Comment[]> {
+    const { skip, take, cursor, where, orderBy, post } = params;
+    return await this.prisma.comment.findMany({
+      skip,
+      take,
+      cursor,
+      where: {
+        post,
+        ...where,
+      },
+      orderBy,
+    });
+  }
+}
