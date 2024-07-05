d40c86248915bb11335dd243d24eb17781afbdc3
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-05 15:55:27 -0400

    created createPost route in post contorller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { CreatePostDto } from './dto/createPost.dto';
+import { Post as PostModel } from '@prisma/client';
+import { PostService } from './post.service';
+import { jwtPayload } from 'src/auth/auth.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @UseGuards(AdminGuard)
+  @Post('create')
+  public async createPost(
+    @Body() createPostDto: CreatePostDto,
+    @Res() res,
+  ): Promise<PostModel> {
+    const user: jwtPayload = res.user;
+    return this.postService.createPost({
+      ...createPostDto,
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    });
+  }
+}
