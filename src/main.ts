2fa8134f044d487687e4b5c0440926d5089b7d0d
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-05 18:04:51 -0400

    added swagger ui to the project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ async function bootstrap() {
       disableErrorMessages: false,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Custom CMS')
+    .setDescription('A custom CMS API Rest')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
