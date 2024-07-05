0445b2d171a6eb44bae8f6cfd9c21b1d6557950e
Cesar-Marcano <marcanocesar86@gmail.com>
2024-07-05 17:25:45 -0400

    created comment service and controller

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CommentController } from './comment.controller';
 
-@Module({})
+@Module({
+  providers: [CommentService],
+  controllers: [CommentController],
+})
 export class CommentModule {}
